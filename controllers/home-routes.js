fb19d7991df94e768070a3484844802e05609a63
rileysong01 <rsong47@uwo.ca>
2023-09-23 13:58:12 -0400

    added static handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,22 +11,25 @@ const {
 // GET all cards for the deck in the homepage
 router.get('/', async (req, res) => {
   try {
-    const dbPlayerDecskData = await PlayerDecks.findAll({
-      include: [
-        {
-          model: Card,
-          attributes: ['name', 'carddDescription'],
-        },
-      ],
-    });
+    // const dbPlayerDecskData = await PlayerDecks.findAll({
+    //   include: [
+    //     {
+    //       model: Card,
+    //       attributes: ['name', 'carddDescription'],
+    //     },
+    //   ],
+    // });
 
-    const playerDecks = dbPlayerDecksData.map((playerDecks) =>
-      playerDecks.get({ plain: true })
-    );
-    res.render('homepage', {
-      playerDecks,
-      loggedIn: req.session.loggedIn,
-    });
+    // const playerDecks = dbPlayerDecskData.map((playerDecks) =>
+    //   playerDecks.get({ plain: true })
+    // );
+    // res.render('homepage', {
+    //   playerDecks,
+    //   loggedIn: req.session.loggedIn,
+    // });
+
+    res.render('homepage');
+
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -34,31 +37,31 @@ router.get('/', async (req, res) => {
 });
 
 // GET one card
-router.get('/playerDecks/:id', async (req, res) => {
-  try {
-    const dbPlayerDecksData = await PlayerDecks.findByPk(req.params.id, {
-      include: [
-        {
-          model: Card,
-          attributes: [
-            'id',
-            'name',
-            'cost',
-            'power',
-            'ability',
-            'cardDescription',
-          ],
-        },
-      ],
-    });
+// router.get('/playerDecks/:id', async (req, res) => {
+//   try {
+//     const dbPlayerDecksData = await PlayerDecks.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: Card,
+//           attributes: [
+//             'id',
+//             'name',
+//             'cost',
+//             'power',
+//             'ability',
+//             'cardDescription',
+//           ],
+//         },
+//       ],
+//     });
 
-    const playerDecks = dbPlayerDecksData.get({ plain: true });
-    res.render('playerdecks', { playerDecks, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+//     const playerDecks = dbPlayerDecksData.get({ plain: true });
+//     res.render('playerdecks', { playerDecks, loggedIn: req.session.loggedIn });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
 // Login route
 router.get('/login', (req, res) => {
