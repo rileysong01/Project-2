76c8b063cfd8eb57538c17e243f241d8c4ca6fd1
Dheeraj Thind <dheerajthind@gmail.com>
2023-09-24 13:32:03 -0400

    exported the routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-const deckRoutes= require('./buildDeck.js');
 
 router.use('/User', userRoutes);
-router.use('/Deck', deckRoutes)
-
 
 module.exports = router;
