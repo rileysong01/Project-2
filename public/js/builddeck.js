758c55d41db86261bdfca31053f3d28ff23f0550
rileysong01 <rsong47@uwo.ca>
2023-09-24 17:35:03 -0400

    refactored buiddeck.js

--- public/js/builddeck.js
+++ public/js/builddeck.js
@@ -12,20 +12,14 @@ document.addEventListener('DOMContentLoaded', function () {
             // Clone the clicked card
             const clonedCard = card.cloneNode(true);
 
-            // Create a new div element with the 'image-item' class
             const imageItemDiv = document.createElement('div');
             imageItemDiv.className = 'image-item';
-
-            // Copy the image and attributes from the cloned card to the new div
             const cardImage = clonedCard.querySelector('img');
 
-            // Set the 'width' style to '100%' instead of 'max-width: 100%'
             cardImage.style.width = '150px';
 
             const imageClone = cardImage.cloneNode(true);
             imageItemDiv.appendChild(imageClone);
-
-            // Append the new div (with image) to the top section
             topSection.appendChild(imageItemDiv);
 
             // Remove the clicked card from the cData picture pool
@@ -34,22 +28,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the "Delete Deck" button element
-    const deleteDeckButton = document.getElementById('deleteDeck');
-
-    // Add a click event listener to the button
-    deleteDeckButton.addEventListener('click', function () {
-        // Refresh the page
-        location.reload();
-    });
-});
-
 
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Get the "Delete Deck" and "Save Deck" buttons
     const deleteDeckButton = document.getElementById('deleteDeck');
     const saveDeckButton = document.getElementById('saveDeck');
 
@@ -70,21 +52,17 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Add a click event listener to the "Delete Deck" button
+
     deleteDeckButton.addEventListener('click', function () {
-        // Refresh the page
         location.reload();
     });
 
-    // Check if the top section is empty and hide the buttons initially
     toggleButtonVisibility();
 
-    // Add an event listener to watch for changes in the top section
     const topSectionObserver = new MutationObserver(function () {
         toggleButtonVisibility();
     });
 
-    // Start observing changes in the top section
     const topSection = document.querySelector('.top-section');
     topSectionObserver.observe(topSection, { childList: true });
 });
