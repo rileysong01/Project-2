871acf949d0e5a45df9093a627064af23c043271
rileysong01 <rsong47@uwo.ca>
2023-09-25 18:58:48 -0400

    switched to jawsdb

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,15 @@
-const seedPlayers = require('./players-seed.js');
-
+// const seedPlayers = require('./players-seed.js');
+const dataEntries = require('./seed.js')
 const sequelize = require('../config/connection.js');
+const Card = require('../models/Card.js')
 
 const seedDB = async () => {
     await sequelize.sync({force:true});
-    console.log('DB SYNCED')
+    // console.log(dataEntries)
 
-    await seedPlayers();
+    const cards = await Card.bulkCreate(dataEntries)
+    console.log(cards )
+    // await seedPlayers();
     console.log(' Players seeded')
 
     process.exit(0);
