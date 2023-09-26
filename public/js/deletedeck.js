e71f49fe60d1b4a69bd89f2f7323fa296475d67f
rileysong01 <rsong47@uwo.ca>
2023-09-26 03:32:58 -0400

    changed to async/await

--- public/js/deletedeck.js
+++ public/js/deletedeck.js
@@ -1,32 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     const deleteButtons = document.querySelectorAll(".delete-deck");
 
-    deleteButtons.forEach(function (button) {
-        button.addEventListener("click", function () {
+    deleteButtons.forEach(async function (button) {
+        button.addEventListener("click", async function () {
             const deckId = button.getAttribute("data-deckid"); // Get the deck ID from data attribute
 
-
-            fetch(`/mydeck/${deckId}`, {
-                method: "DELETE",
-            })
-                .then(function (response) {
-                    if (response.ok) {
-
-                        console.log(`Deck ${deckId} deleted successfully.`);
-
-                        const deckContainer = document.querySelector(`.deckcontainer[data-deckid="${deckId}"]`);
-                        if (deckContainer) {
-                            deckContainer.remove();
-                        }
-                    } else {
-
-                        console.error(`Error deleting deck ${deckId}: ${response.status}`);
-                    }
-                })
-                .catch(function (error) {
-
-                    console.error(`Error deleting deck ${deckId}:`, error);
+            try {
+                const response = await fetch(`/mydeck/${deckId}`, {
+                    method: "DELETE",
                 });
+
+                if (response.ok) {
+                    console.log(`Deck ${deckId} deleted successfully.`);
+                    const deckContainer = document.querySelector(`.deckcontainer[data-deckid="${deckId}"]`);
+                    if (deckContainer) {
+                        deckContainer.remove();
+                    }
+                } else {
+                    console.error(`Error deleting deck ${deckId}: ${response.status}`);
+                }
+            } catch (error) {
+                console.error(`Error deleting deck ${deckId}:`, error);
+            }
         });
     });
 });
